b42134738bce5dfba61d0dbe61eb37dc171c5e65
Karthik Sridhar <karthiksridhar22@gmail.com>
2024-11-25 07:12:05 +0530

    changes to the way delays are logged

--- tests/client_test.py
+++ tests/client_test.py
@@ -1,7 +1,7 @@
 from src.client_architecture import Client
 import time
 
-client = Client(identifier='client1', servers={'server1': ('127.0.0.1', 9001), 'server2': ('127.0.0.1', 9002), 'server3': ('127.0.0.1', 9003)})
+client = Client(identifier='client1', servers={'server1': ('127.0.0.1', 9101), 'server2': ('127.0.0.1', 9102), 'server3': ('127.0.0.1', 9103)})
 client.start()
   #keep the main program running to allow command input
 try:
--- tests/server_test_3.py
+++ tests/server_test_3.py
@@ -2,9 +2,9 @@ from src.server_architecture import Server
 import time
 
 if __name__ == "__main__":
-  server3 = Server('127.0.0.1', 9003, peers={
-      'server1': ('127.0.0.1', 9001),
-      'server2': ('127.0.0.1', 9002)
+  server3 = Server('127.0.0.1', 9103, peers={
+      'server1': ('127.0.0.1', 9101),
+      'server2': ('127.0.0.1', 9102)
   }, identifier='server3')
   server3.start()
   #keep the main program running to allow command input
